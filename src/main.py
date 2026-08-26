49d3e5a6338df6019567198627d51c127dc62ccf
Sapna <sapnasingh00127@gmail.com>
2026-08-26 21:41:05 +0530

    Implement basic command-line task manager

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from agent import TaskPilot
+
+agent = TaskPilot()
+
+while True:
+    command = input("You: ")
+
+    if command.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    elif command.lower().startswith("add "):
+        task = command[4:]
+        print(agent.add_task(task))
+
+    elif command.lower() == "tasks":
+        print(agent.show_tasks())
+
+    else:
+        print("Try: add <task> or tasks")
