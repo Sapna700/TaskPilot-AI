08e346f4395da073b09359e07cc6be81fef1f57e
Sapna <sapnasingh00127@gmail.com>
2026-08-31 20:37:21 +0530

    Add get_available_tools function to tools.py

---
+++ src/tools.py
@@ -0,0 +1,6 @@
+def get_available_tools():
+    return {
+        "add_task": "Add a new task",
+        "view_tasks": "View all saved tasks",
+        "complete_task": "Mark a task as completed"
+    }
